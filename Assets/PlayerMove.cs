b758066670796834431e26347d99bbf50e2837de
Peter Wells <petethebeatwells@gmail.com>
2020-08-24 12:41:05 -0700

    first real commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    public float moveSpeed = 10f;
+    
+    float horizontal;
+    float vertical;
+
+    float moveLimiter = .7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+
+        body.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+    }
+}
